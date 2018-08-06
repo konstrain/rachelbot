20bc69a489bdfd76671c8d3aa473ec6be20eb5d7
konstrain <39788968+konstrain@users.noreply.github.com>
2018-08-06 10:55:58 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -213,7 +213,7 @@ bot.on('message',  message => {
                       channel.send({
                         embed: {
                           color:9871541,
-                          description: "Information has not been updated. There are some issues and the numbers are not tallying with nexon ones.",
+                          description: "S2 and S3 information are not updated. There are some issues and the numbers are not tallying with Nexon ones.",
                           fields: [
                             {
                               "name": "US Mini-Mini",
