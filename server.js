0e62ed1fbbf1d8d43c2ebcc2eba8aa1402f60168
konstrain <39788968+konstrain@users.noreply.github.com>
2022-09-14 16:09:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,58 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const https = require('https');
+const Discord = require('discord.js');
+const auth = require('./auth.json');
 
-client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-})
+const url = "https://icanhazdadjoke.com/slack";
 
-client.on("message", msg => {
-  if (msg.content === "ping") {
-    msg.reply("pong");
+const randomizeJoke = (channel) => {
+  try {
+      https.get(url, res => {
+          res.setEncoding("utf8");
+
+          let body = "";
+          res.on("data", chunk => {
+              body += chunk;
+          });
+
+          res.on("end", () => {
+            body = JSON.parse(body);
+
+            //console.log(`Result: ${body.attachments[0].fallback}`); // for debug purposes
+            channel.send({ 
+              embed: {
+                color:3447003,
+                title: "Are you ready, Captain?",
+                description:body.attachments[0].fallback
+              }
+            });
+          });
+      }).on('error', (e) => {
+        console.error(e);
+        channel.send(e);
+      });
+    } catch (err) {
+    console.error(err);
   }
-})
+}
 
-client.login(process.env.TOKEN);
+bot.on('message', async message => {
+  
+  var channel = message.channel;
+ 
+  switch (message.content) {
+    case "ping":
+      message.reply("Pong!");
+      break;
+    case "!joke":
+      message.channel.send("Here's your joke!");
+      randomizeJoke(channel);
+      break;
+   }
+}).on('error', (e) => {
+      console.error(e);
+    });
+
+
+process.on('unhandledRejection', console.error);
+
+bot.login(process.env.BOT_TOKEN);
