fc3b873ceaff9434da60bd1f56395483923e20bb
konstrain <39788968+konstrain@users.noreply.github.com>
2026-04-03 23:03:08 +0800

    Update server.js

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ bot.on('messageCreate', async message => {
             break;
 
         case "love":
-            if (message.author.id === OWNER_ID)
+            if (message.author.id === OWNER_ID) {
                 channel.send("Do you even need to ask? It's definitely you!");
             } else {
                 channel.send("The one and only man I love... <@350550564527931392> My life, my love, my hubby.");
