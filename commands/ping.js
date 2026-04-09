5988027426c42d8f37a3823ab97a2aa62800937d
konstrain <39788968+konstrain@users.noreply.github.com>
2026-04-09 17:22:30 +0800

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,71 @@
 module.exports = {
     name: "ping",
-    description: "Checks if I'm alive or not.",
-    execute: (message, args, channel) => {
-        channel.send("Don't ping me, captain. Don't r!poke me too... :flushed:");
+    description: "Check if Rachel is responsive.",
+
+    execute: (message, args, channel, context) => {
+
+        const { OWNER_ID, getMood } = context;
+
+        const mood = getMood();
+        const isOwner = message.author.id === OWNER_ID;
+
+        const responses = {
+            cheerful: {
+                owner: [
+                    "I'm always here for you, Captain 😊",
+                    "Pong~ you called?",
+                    "Right here. Watching you 👀"
+                ],
+                user: [
+                    "Pong! 😊",
+                    "I'm here!",
+                    "Yes yes, alive and well."
+                ]
+            },
+
+            playful: {
+                owner: [
+                    "Miss me already? 😏",
+                    "Pong~ what do you need, Captain?",
+                    "You checking up on me again?"
+                ],
+                user: [
+                    "Pong~ 😏",
+                    "You called?",
+                    "At your service. Maybe."
+                ]
+            },
+
+            sarcastic: {
+                owner: [
+                    "Yes, Captain. I’m still functioning.",
+                    "Congratulations. Your bot works.",
+                    "You built me just to test ping?"
+                ],
+                user: [
+                    "Yes, I'm alive.",
+                    "Congratulations. It works.",
+                    "What a shocking result."
+                ]
+            },
+
+            sleepy: {
+                owner: [
+                    "...pong 😴",
+                    "Captain… must you test me now…",
+                    "I was resting…"
+                ],
+                user: [
+                    "...pong 😴",
+                    "you woke me up",
+                    "present… barely"
+                ]
+            }
+        };
+
+        const pool = isOwner ? responses[mood].owner : responses[mood].user;
+        const reply = pool[Math.floor(Math.random() * pool.length)];
+
+        channel.send(reply);
     }
 };
